34228b88bd39480861a41dc4eaac7cc187563684
Jurfest <jurfest.apps@gmail.com>
2020-02-08 11:53:53 -0300

    recipe_detail page update

--- public/scripts.js
+++ public/scripts.js
@@ -4,7 +4,8 @@ const recipes = document.querySelectorAll('.recipe')
 for (let recipe of recipes) {
   recipe.addEventListener("click", function () {
     const recipeId = recipe.getAttribute('id'); // or just: recipe.id
-    console.log(recipeId);
+    // const recipeNumber = recipes.findIndex(recipeId);
+    console.log(`id da receita: ${recipeId}`);
   })
 }
 
--- server.js
+++ server.js
@@ -9,6 +9,12 @@ server.use(express.static('public'));
 
 server.set("view engine", "njk");
 
+nunjucks.configure("views", {
+  express: server,
+  autoescape: false,
+  noCache: true
+});
+
 server.get('/', function (req, res) {
   return res.render('home', { recipes: recipes });
 });
@@ -21,17 +27,17 @@ server.get('/about', function (req, res) {
   return res.render('about');
 });
 
-nunjucks.configure("views", {
-  express: server,
-  autoescape: false,
-  noCache: true
-});
-
 server.get("/recipes/:index", function (req, res) {
   const recipeIndex = req.params.index;
-
-  console.log(recipes[recipeIndex]);
+  const recipe = recipes[recipeIndex];
+  console.log(recipe);
+  return res.render('recipe_detail', { recipe: recipe })
 })
+
+// Atention: this error route must be below all other routes
+server.use(function (req, res) {
+  res.status(404).render("not-found");
+});
 
 server.listen(5000, function() {
   console.log('server is running');
