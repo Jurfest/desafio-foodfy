d84b082c66f3873788823a469345b4316f75b037
Jurfest <jurfest.apps@gmail.com>
2020-02-09 18:01:21 -0300

    recipe_detail update with button show/hide

--- public/scripts.js
+++ public/scripts.js
@@ -1,29 +1,13 @@
-// const modalOverlay = document.querySelector('.modal-overlay')
-// const recipes = document.querySelectorAll('.recipe')
+function Mudarestado(el, btn) {
+  var display = document.getElementById(el).style.display;
+  if (display == "none")
+    document.getElementById(el).style.display = 'block';
+  else
+    document.getElementById(el).style.display = 'none';
 
-// for (let recipe of recipes) {
-//   recipe.addEventListener("click", function () {
-//     const recipeId = recipe.getAttribute('id'); // or just: recipe.id
-//     // const recipeNumber = recipes.findIndex(recipeId);
-//     console.log(`id da receita: ${recipeId}`);
-//   })
-// }
-
-
-const button = document.querySelector('.button');
-
-button.addEventListener("click", function () {
-  button.textContent = "MOSTRAR"
-
-  //  var display = document.getElementById(el).style.display;
-  //  if (display == "none")
-  //    document.getElementById(el).style.display = 'block';
-  //  else
-  //    document.getElementById(el).style.display = 'none';
-});
-
- 
-
-
-// button.classList.remove('active')
-// button.querySelector('iframe').src = ""
+  var showOrHide = document.getElementById(btn).textContent;
+  if (showOrHide === "MOSTRAR")
+    document.getElementById(btn).textContent = 'ESCONDER';
+  else
+    document.getElementById(btn).textContent = 'MOSTRAR';
+}
